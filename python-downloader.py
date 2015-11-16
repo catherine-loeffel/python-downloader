70381b5096823305ce8b61b9bcb55a4121e3e0cc
Catherine Loeffel <catherine@loeffel.de>
2015-11-16 21:53:19 +0100

    added support for reading file as argument

--- python-downloader.py
+++ python-downloader.py
@@ -1,19 +1,29 @@
+from sys import argv
 import urllib
 
 outputFolder = './output/'
-file = open('examplelinks.txt')
+inputfile = 'examplelinks.txt'
 
-print 'Running Python Downloader...'
-with file as f:
-    for line in f:
-        filename = outputFolder + line.rsplit('/', 1)[-1].strip()
-        print '\ntrying to download: ' + line.strip()
-        try:
-            urllib.urlretrieve(line, filename)
-            print 'saved as ' + filename
-        except IOError:
-            print 'could not find file'
-            pass
+if len(argv) == 2:
+    inputfile = argv[1]
+
+try:
+    file = open(inputfile)
+    print 'Running Python Downloader...'
+    with file as f:
+        for line in f:
+            filename = outputFolder + line.rsplit('/', 1)[-1].strip()
+            print '\ntrying to download: ' + line.strip()
+            try:
+                urllib.urlretrieve(line, filename)
+                print 'saved as ' + filename
+            except IOError:
+                print 'could not find file'
+                pass
+except IOError:
+    print 'could not open file: ' + inputfile
+
+
 
 
 
